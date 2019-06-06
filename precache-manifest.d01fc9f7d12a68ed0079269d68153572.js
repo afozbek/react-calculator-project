b3b55c5053a0e171dba505fd530ca802423a72e0
afozbek <furkanozbek1995@gmail.com>
2019-06-06 15:21:24 +0300

    Updates

---
+++ precache-manifest.d01fc9f7d12a68ed0079269d68153572.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acb28c6dce42700c122d74c1674afe7d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "905a920aee633e55c59c",
+    "url": "/static/css/main.a1afa5f2.chunk.css"
+  },
+  {
+    "revision": "1435a656bfa271adfc95",
+    "url": "/static/js/2.0c22ad49.chunk.js"
+  },
+  {
+    "revision": "905a920aee633e55c59c",
+    "url": "/static/js/main.1ed8c206.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
